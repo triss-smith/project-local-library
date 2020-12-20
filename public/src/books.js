bb28063ef83b3619bc47a3c67a79bd8ba013da2a
triscsmith <44349470+triscsmith@users.noreply.github.com>
2020-12-20 07:20:14 -0600

    partitionBooksByBorrowsStatus fix
    Got it working

--- public/src/books.js
+++ public/src/books.js
@@ -6,7 +6,23 @@ function findBookById(books, id) {
   return books.find((array) => array.id === id)
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let booksOut = [];
+  let booksAvailable = [];
+  let booksTotalArray = []
+  for(let i = 0; i < books.length; i++) {
+    if(books[i].borrows[0].returned) {
+      booksAvailable.push(books[i])
+    }
+    else {
+      booksOut.push(books[i])
+    }
+  }
+  console.log(booksOut)
+  booksTotalArray.push(booksOut)
+  booksTotalArray.push(booksAvailable)
+  return booksTotalArray;
+}
 
 function getBorrowersForBook(book, accounts) {}
 
